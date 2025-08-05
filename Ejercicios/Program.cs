42bce43adc4597223f902dfc2672aff133155afc
Fco Gomez <Fco Gomez@DESKTOP-VJBN7NL>
2025-08-04 23:44:12 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double costo = 10;
+            Console.Write($"Costo por hora: {costo}" + " ");
+
+            double horas = 8;
+            Console.Write($"Horas trabajadas: {horas}" + " ");
+
+
+            double total = costo * horas;
+
+            Console.WriteLine("Total devengado: " + total);
+        }
+    }
+}
